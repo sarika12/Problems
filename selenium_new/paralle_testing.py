ff51874a43c3b3d370c9315007b8f3c2acb204db
sarika12 <sarika1989.08@gmail.com>
2025-03-26 12:12:03 +0530

    parallel execution in python .

---
+++ selenium_new/paralle_testing.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+
+# option=Options()
+# option.add_experimental_option("detach",True)
+# driver=webdriver.Chrome(options=option)
+# driver.get("")
+# webdriver.FirefoxOptions()
+
+@pytest.mark.parametrize("browser",["Chrome","Edge"])
+def test_parallel_execution(browser):
+    if browser=="Chrome":
+        options=Options()
+        driver=webdriver.Chrome(options=options)
+    # elif browser=="FireFox":
+    #     driver=webdriver.Firefox()
+    elif browser=="Edge":
+        driver=webdriver.Edge()
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+    driver.quit()
